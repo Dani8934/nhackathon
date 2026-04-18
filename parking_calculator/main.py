bc047383159ee519d5ee577147f20a968ea15c51
Bencs Dániel <bencsdani0529@gmail.com>
2026-04-18 02:59:24 +0200

    Parkolás díjszámító visszaállítás

--- parking_calculator/main.py
+++ parking_calculator/main.py
@@ -44,7 +44,7 @@ def main():
     data = Path("input.txt").read_text(encoding="utf-8")
     for line in data.splitlines()[2:]:
         line = line.split("\t\t")
-        print(f"{line[0]}\t\t{parking_fee(line[1], line[2])} Ft", end="\n")
+        print(f"{line[0]}\t\t{parking_fee(line[1], line[2])}", end="\n")
 
 
 if __name__ == "__main__":
